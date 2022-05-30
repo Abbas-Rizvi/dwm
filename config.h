1566e0b6e1d48ceaf99cd4488689fb3bfb95a6a5
Abbas R <abbas8rizvi@gmail.com>
2022-05-30 17:05:39 -0400

    Changed Colors

--- config.h
+++ config.h
@@ -1,22 +1,30 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Mono:size=11:antialias=true:autohint=true", "Noto Color Emoji-11"};
+static const char *fonts[]          = { "Liberation Mono Regular:size=11:antialias=true:autohint=true", "Noto Color Emoji-11"};
 static const char dmenufont[]       = "SF Mono-11";
+
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#005577";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_purp[]        = "#75538C";
+static const char col_red[]        = "#C20F0F";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_purp,  col_purp  },
 };
 
 /* tagging */
@@ -28,8 +36,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
+	{ "strawberry",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "discord",     NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* window swallowing */
@@ -63,11 +72,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purp, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "librewolf", NULL };
 static const char *lockcmd[]  = { "xlock", "-mode", "rain", NULL };
-
+static const char *screenshot[]  = { "gscreenshot-cli", "-sc", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -75,6 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,		            	XK_w,	   spawn, 	   {.v = browsercmd} },
 	{ MODKEY|ShiftMask,		        XK_l,	   spawn,   	   {.v = lockcmd} },
+	{ 0,                       XK_Print,  spawn,          {.v = screenshot}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
