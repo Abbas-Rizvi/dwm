fe6068191c43efaf7d48168b7843de71be20e1e8
Abbas R <abbas8rizvi@gmail.com>
2022-09-17 15:48:48 -0400

    updated window auto-assignments

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Mono Regular:size=11:antialias=true:autohint=true", "Noto Color Emoji-11"};
+static const char *fonts[]          = { "Liberation Mono Regular:size=11:antialias=true:autohint=true", "JoyPixels-11"};
 static const char dmenufont[]       = "SF Mono-11";
 
 // static const char col_gray1[]       = "#222222";
@@ -19,7 +19,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_purp[]        = "#75538C";
-static const char col_red[]        = "#C20F0F";
+static const char col_red[]         = "#C20F0F";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -28,17 +28,20 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "👽" , "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
-	{ "strawberry",  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "discord",     NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+//	{ "strawberry",     NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "qBittorrent",    NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Hexchat",        NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "Steam",          NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "discord",        NULL,       NULL,       1 << 8,       0,           -1 }
 };
 
 /* window swallowing */
