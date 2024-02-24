48c0be0ffca793522a5bb9efd7a79afd072c620a
Abbas Rizvi <abbas@abbasr.ca>
2024-02-24 00:57:14 -0500

    removed un-needed files, removed un-needed keybinds

--- config.h
+++ config.h
@@ -108,8 +108,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_Background, "-nf", col_White, "-sb", col_Cur_Line, "-sf", col_White, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "librewolf", NULL };
-static const char *lockcmd[]  = { "xlock", "-mode", "rain", NULL };
-static const char *screenshot[]  = { "gscreenshot-cli", "-sc", NULL };
+// static const char *lockcmd[]  = { "xlock", "-mode", "rain", NULL };
+static const char *lockcmd[]  = { "slock",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -117,7 +117,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,		            	XK_w,	   spawn, 	   {.v = browsercmd} },
 	{ MODKEY|ShiftMask,		        XK_l,	   spawn,   	   {.v = lockcmd} },
-	{ 0,                       XK_Print,  spawn,          {.v = screenshot}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
